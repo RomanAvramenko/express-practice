464355e798762e3bfcdcc1505cb92e11008e206c
RomanAvramenko <ashkaell24@gmail.com>
2021-01-10 16:43:09 +0200

    Added mongoDB and mongoose

--- index.js
+++ index.js
@@ -1,16 +1,22 @@
 const express = require("express");
+const mongoose = require("mongoose");
+const path = require("path");
+const Handlebars = require("handlebars");
 const exphbs = require("express-handlebars");
+const {
+  allowInsecurePrototypeAccess,
+} = require("@handlebars/allow-prototype-access");
 const homeRoutes = require("./routes/home");
 const cardRoutes = require("./routes/card");
 const addRoutes = require("./routes/add");
 const coursesRoutes = require("./routes/courses");
-const path = require("path");
 
 const app = express();
 
 const hbs = exphbs.create({
   defaultLayout: "main",
   extname: "hbs",
+  handlebars: allowInsecurePrototypeAccess(Handlebars),
 });
 
 app.engine("hbs", hbs.engine);
@@ -26,6 +32,19 @@ app.use("/card", cardRoutes);
 
 const PORT = process.env.PORT || 3000;
 
-app.listen(PORT, () => {
-  console.log(`Server is running on port ${PORT}`);
-});
+async function start() {
+  try {
+    const url = `mongodb+srv://roman:3gNi98C6wNDw46w@cluster0.udulk.mongodb.net/shop`;
+    await mongoose.connect(url, {
+      useNewUrlParser: true,
+      useUnifiedTopology: true,
+    });
+    app.listen(PORT, () => {
+      console.log(`Server is running on port ${PORT}`);
+    });
+  } catch (e) {
+    console.log(e);
+  }
+}
+
+start();
--- routes/courses.js
+++ routes/courses.js
@@ -1,41 +1,43 @@
-const {Router} = require('express')
-const Course = require('../models/course')
-const router = Router()
+const { Router } = require("express");
+const Course = require("../models/course");
+const router = Router();
 
-router.get('/', async (req, res) => {
-  const courses = await Course.getAll()
-  res.render('courses', {
-    title: 'Курсы',
+router.get("/", async (req, res) => {
+  const courses = await Course.find();
+  res.render("courses", {
+    title: "Курсы",
     isCourses: true,
-    courses
-  })
-})
+    courses,
+  });
+});
 
-router.get('/:id/edit', async (req, res) => {
+router.get("/:id/edit", async (req, res) => {
   if (!req.query.allow) {
-    return res.redirect('/')
+    return res.redirect("/");
   }
 
-  const course = await Course.getById(req.params.id)
+  const course = await Course.findById(req.params.id);
 
-  res.render('course-edit', {
+  res.render("course-edit", {
     title: `Редактировать ${course.title}`,
-    course
-  })
-})
+    course,
+  });
+});
 
-router.post('/edit', async (req, res) => {
-  await Course.update(req.body)
-  res.redirect('/courses')
-})
+router.post("/edit", async (req, res) => {
+  const { id } = req.body;
+  delete req.body.id;
+  await Course.findByIdAndUpdate(id, req.body);
+  res.redirect("/courses");
+});
 
-router.get('/:id', async (req, res) => {
-  const course = await Course.getById(req.params.id)
-  res.render('course', {
-    layout: 'empty',
+router.get("/:id", async (req, res) => {
+  const course = await Course.findById(req.params.id);
+  res.render("course", {
+    layout: "empty",
     title: `Курс ${course.title}`,
-    course
-  })
-})
+    course,
+  });
+});
 
-module.exports = router
+module.exports = router;
